c82f1b34c6cababd37f1bcb75c644b7f215c9f32
qdpdwjx <105757700+qdpdwjx@users.noreply.github.com>
2022-12-09 16:01:27 +0800

    Update qiuxia.js

--- JS/qiuxia.js
+++ JS/qiuxia.js
@@ -1,8 +1,10 @@
-var rule= {
-模板:'mxone5',
-title:'量子影视',
-host:'http://www.lzizy9.com',
-url:'/index.php/vod/show/id/fyclass/page/fypage.html',
-searchUrl:'/index.php/vod/search.html?wd=**',
-cate_exclude:'网址'
-}
+muban.首图2.二级.tabs = '.stui-pannel__head.bottom-line.active.clearfix h3';
+muban.首图2.二级.content = '.stui-content__desc&&Text';
+var rule = Object.assign(muban.首图2,{
+title:'秋霞',
+host:'https://www.7xiady.cc',
+url:'/type/fyclass-fypage/',
+class_parse:'.stui-header__menu li;a&&Text;a&&href;/type/(.*?)/',
+searchUrl:'/search/**----------fypage---.html',
+搜索:'ul.stui-vodlist&&li;a&&title;.lazyload&&data-original;.pic-text&&Text;a&&href',
+});
