8bc81d710f0afa4388c6937348c0c36080661c12
qdpdwjx <105757700+qdpdwjx@users.noreply.github.com>
2023-01-09 15:30:29 +0800

    Update jrkan.js

--- JS/jrkan.js
+++ JS/jrkan.js
@@ -1,15 +1,12 @@
 var rule = {
-    title:'JRKAN直播',
-    host:'http://m.jrkan2023.com',
-	// JRKAN备用域名:m.jrskan8.com / m.jrkan666.com / jryyds.com / jrsbxj.com
-	// JRKAN网址发布:qiumi1314.com
+    title:'JRS看直播',
+    host:'http://www.jrkan2023.com/',
     url:'/fyclass',
     searchUrl:'',
     searchable:0,
     quickSearch:0,
-    class_name:'球赛直播',
+    class_name:'全部',
     class_url:'/',
-    //class_url:'?live',
     headers:{
         'User-Agent':'MOBILE_UA'
     },
@@ -18,13 +15,8 @@ var rule = {
     lazy:'',
     limit:6,
     double:false,
-    timeout:5000,
-    play_parse:true,
-    lazy:'',
-    limit:6,
-    double:false,
     推荐:'*',
-    一级:'.loc_match:eq(2) ul;li:gt(1):lt(4)&&Text;img&&src;li:lt(2)&&Text;a:eq(1)&&href',
-   二级:{title:'.sub_list li:lt(2)&&Text;.sub_list li:eq(0)&&Text',img:'img&&src',desc:';;;.lab_team_home&&Text;.lab_team_away&&Text',content:'.sub_list ul&&Text',tabs:'',tab_text:'',lists:'.sub_channel a',list_text:'a&&Text',list_url:'a&&data-play'},
+    一级:'.row a;li div:gt(2):lt(6)&&Text;img&&src;.match-start-time&&Text;a&&href',
+    二级:{title:'.live-title&&Text;.live-title&&Text',img:'img:eq(1)&&src',desc:';;;.live-left-team-sj&&Text;.live-right-team-sj&&Text',content:'.live-info&&Text',tabs:'',tab_text:'',lists:'#item-source a',list_text:'a&&Text',list_url:'a&&data-url'},
     搜索:'',
 }
