050f396790520efacd6dd414b1c87ad4eebd1246
qdpdwjx <105757700+qdpdwjx@users.noreply.github.com>
2023-05-15 18:50:59 +0800

    Update mao.php

--- mao.php
+++ mao.php
@@ -140,9 +140,10 @@ CHC动作电影,http://dbiptv.sn.chinamobile.com/PLTV/88888890/224/3221226465/in
 CHC电影,http://118.24.227.124/__cl/cg:live/__c/chcgqdyHD/__op/default/__f/index.m3u8
 CHC动作电影,http://118.24.227.124/__cl/cg:live/__c/chcdzdyHD/__op/default/__f/index.m3u8
 CHC家庭影院,http://118.24.227.124/__cl/cg:live/__c/chcjtyyHD/__op/default/__f/index.m3u8
-TVBS新聞,http://2022.stream-link.org/list.php?id=t013
-寰宇國際新聞,http://2022.stream-link.org/list.php?id=t007
-寰宇台灣新聞,http://2022.stream-link.org/list.php?id=t008
-民視新聞,http://2022.stream-link.org/list.php?id=t006
-中天新聞,http://2022.stream-link.org/list.php?id=t003
-金马影院,http://hpull.kktv8.com/livekktv/128600025/playlist.m3u8
+TVBS新闻,https://pull-l3-cny.douyincdn.com/live/YesLiveTV-300d09ed-7cca-4909-831e-3184bf30496f.m3u8?by=yeslivetv_com
+TVBS新闻,http://154.216.191.59/ycdz/x.php?id=54
+TVBS HD,http://154.216.191.59/ycdz/x.php?id=56
+TVBS HD,https://pull-l3-cny.douyincdn.com/live/YesLiveTV-71e43e9f-ffec-42ac-b63a-38c5f7ef6ec1.m3u8?by=yeslivetv_com
+寰宇新闻,http://154.216.191.59/ycdz/x.php?id=53
+民視新聞,http://154.216.191.59/ycdz/x.php?id=50
+中天新聞,http://154.216.191.59/ycdz/x.php?id=43
